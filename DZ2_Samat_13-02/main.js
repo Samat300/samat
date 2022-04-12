334f8c012a0a4c8a362bbb2f68ec645703fba8e1
Samat300 <keneshovsamat304@gmail.com>
2022-04-12 22:19:17 +0600

    DZ2_Samat_13-02

---
+++ DZ2_Samat_13-02/main.js
@@ -0,0 +1,13 @@
+const obj = {
+    key1:'*',
+    key2:'**',
+    key3:'***',
+    key4:'****',
+    key5:'*****',
+    key6:'******',
+    key7:'*******',
+}
+for (let key in obj){
+    console.log(obj[key])
+}
+
