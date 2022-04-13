3f374f5141307d8f5c8b71617fc2c6e21f7eca29
Samat300 <keneshovsamat304@gmail.com>
2022-04-13 21:53:39 +0600

    lesson3

---
+++ lesson3/main.js
@@ -0,0 +1,51 @@
+// function sayHello() {
+//     console.log('Hello')
+// }
+// sayHello();
+
+// function getFullName(user, age) {
+//     console.log(user, age)
+// }
+//
+// const user = {
+//     first_name: "John",
+//     last_name: "Alex"
+// }
+//
+// getFullName(user, 19)
+
+// const anonym = function (user, age) {
+//     console.log("nothing", user, age)
+// }
+//
+// anonym();
+
+
+
+function nam(h1, h2) {
+    if (h1 > h2){
+        console.log("h1 > h2")
+    }
+    else if (h1 < h2){
+        console.log('h1 < h2')
+    } else {
+        console.log('h1 < h2')
+    }
+}
+nam(7, 8)
+
+
+
+function twoMass(arrayOne, arrayTwo) {
+    if (arrayOne.length > arrayTwo.length){
+    console.log("arrayOne > arrayTwo")
+    } else if (arrayOne.length < arrayTwo.length){
+        console.log("arrayTwo > arrayOne")
+    }else {
+        console.log("массивы одинаковы")
+    }
+}
+twoMass([1,2,3,4,5], [1,2,3,4,5,6])
+
+
+
